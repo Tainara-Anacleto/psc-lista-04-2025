b1841520458f994779d70f608c41e072c3d40e39
Tainara-Anacleto <taanacleto.pires94@gmail.com>
2025-04-04 00:41:51 -0300

    Add files via upload

---
+++ NumeroImpar.java
@@ -0,0 +1,14 @@
+public class NumeroImpar {
+    public static void main(String[] args){
+
+        int numero;
+
+        for(numero = 1; numero <= 50; numero++){
+            if (numero % 2 != 0){
+                System.out.println(numero);
+            }
+        }
+
+    }
+    
+}
